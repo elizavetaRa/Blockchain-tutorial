443d18ed215adac712cf32be25e38001f9266edf
haakondf <haakondf@gmail.com>
2018-09-27 19:53:53 +0200

    added data toblockchain json and introduction

--- src/app/js/Introduction.jsx
+++ src/app/js/Introduction.jsx
@@ -8,6 +8,16 @@ const Introduction = props => {
     return (
         <div>
             <h1>Introduction</h1>
+           
+                <p>
+                This web application will guide you through the basic elements of a blockchain. Follow the learning steps to get your blockchain certificate. Keep in mind that you can always access the Terminology page in the nav bar if you forgot some of the terms, but in order to get the blocktificate you must walk through all the steps. 
+
+The blockchain illustration on step two is retrieving block-data from the SIGNAL-coin blockchain. 
+
+This website is built as a project for the Ironhack bootcamp. 
+
+                </p>
+            
             <Terminology data = {props.data}/>
             <div className="information"></div>
             
